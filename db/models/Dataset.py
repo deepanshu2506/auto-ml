c40c35c2313685c977c917500ecee385c3f8c374
Deepanshu Vangani <vanganideepanshu@gmail.com>
2021-07-22 12:54:47 +0530

    dataset model

---
+++ db/models/Dataset.py
@@ -0,0 +1,49 @@
+from mongoengine.base.fields import ObjectIdField
+from mongoengine.document import EmbeddedDocument
+from utils.enums import DataTypes, DatasetType
+from mongoengine.fields import (
+    EmbeddedDocumentField,
+    EmbeddedDocumentListField,
+    EnumField,
+    FloatField,
+    IntField,
+    ListField,
+    StringField,
+)
+
+
+from mongoengine import Document
+
+
+class DatasetFeatureMetrics(EmbeddedDocument):
+    min = FloatField()
+    max = FloatField()
+    mean = FloatField()
+    median = FloatField()
+    stdDeviation = FloatField()
+    uniqueValues = IntField()
+    missingValues = IntField()
+    samples = ListField(StringField(), max_length=5)
+
+
+class DatasetFeature(EmbeddedDocument):
+    columnOrder = IntField()
+    columnName = StringField()
+    dataType = EnumField(DataTypes)
+    colmnDescription = StringField()
+    metrics = EmbeddedDocumentField(DatasetFeatureMetrics)
+    pass
+
+
+class DatasetInfo(EmbeddedDocument):
+    fileSize = FloatField()
+    tupleCount = IntField()
+
+
+class Dataset(Document):
+    type = EnumField(DatasetType, default=DatasetType.CSV)
+    name = StringField()
+    datasetLocation = StringField()
+    datasetFields = EmbeddedDocumentListField(DatasetFeature)
+    info = EmbeddedDocumentField(DatasetInfo)
+    createdBy = ObjectIdField()
---
+++ utils/enums.py
@@ -0,0 +1,11 @@
+import enum
+
+
+class DatasetType(enum.Enum):
+    CSV = "csv"
+    SQL = "sql"
+
+
+class DataTypes(enum.Enum):
+    NUMBER = "NUMBER"
+    STRING = "STRING"
