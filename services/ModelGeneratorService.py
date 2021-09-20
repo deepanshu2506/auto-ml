b816f4841d2b603db6a37d75390a99083c6d0309
Deepanshu Vangani <vanganideepanshu@gmail.com>
2021-09-20 15:37:27 +0530

    modelgenerator class blueprint

---
+++ services/ModelGeneratorService.py
@@ -0,0 +1,33 @@
+from lib.Logger.Logger import Logger
+
+
+class ModelGenerator:
+    def __init__(self, dataset_id, target_feature, logger: Logger) -> None:
+        self.dataset_id = dataset_id
+        self.target_feature = target_feature
+        self.logger = logger
+        self.results = []
+
+    def fit():
+        pass
+
+    def _get_dataset():
+        pass
+
+    def _preprocess():
+        pass
+
+    def _get_metadata():
+        pass
+
+    def _generate_configuration():
+        pass
+
+    def _run_experiment():
+        pass
+
+    def _cross_validate():
+        pass
+
+    def _select_best_model():
+        pass
