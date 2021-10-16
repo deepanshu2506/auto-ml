4dcd5fcd02cd9c13f060bacb26819f1bba5e9183
Deepanshu Vangani <vanganideepanshu@gmail.com>
2021-10-16 16:50:48 +0530

    save model directory in config object

--- config.py
+++ config.py
@@ -11,3 +11,5 @@ class Config:
     JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
 
     UPLOAD_DIRECTORY = os.environ.get("UPLOAD_DIR") or "./uploads"
+
+    MODEL_SAVE_DIRECTORY = os.environ.get("MODEL_SAVE_DIR") or "./models"
