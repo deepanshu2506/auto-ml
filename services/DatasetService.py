209c3916694ccf4bcd580417c8cf9ab036df2b35
Deepanshu Vangani <vanganideepanshu@gmail.com>
2021-10-19 13:20:06 +0530

    fix aggregation when no conditional operator is used

--- services/DatasetService.py
+++ services/DatasetService.py
@@ -7,13 +7,19 @@ from utils.enums import AggregationMethods, Coltype, DataTypes, DatasetStates, J
 from utils.exceptions import DatasetNotFound
 import numpy
 from pandas.core.series import Series
-from utils.pdUtils import build_query, get_col_type, get_datatype, perform_aggregation,is_discrete_auto_impute
+from utils.pdUtils import (
+    build_query,
+    get_col_type,
+    get_datatype,
+    perform_aggregation,
+    is_discrete_auto_impute,
+)
 from flask_jwt_extended.utils import get_jwt_identity
 from services.FileService import FileService
 from pandas import DataFrame
 import numpy as np
 from werkzeug.datastructures import FileStorage
-import random 
+import random
 from db.models.Dataset import (
     Dataset,
     DatasetFeature,
@@ -140,6 +146,7 @@ class DatasetService:
             filter_query = build_query(filter)
             print(filter_query)
             dataset_frame: DataFrame = dataset_frame.query(filter_query)
+
         aggregated_df = perform_aggregation(
             dataset_frame.groupby(groupby_field)[aggregate_by_field],
             aggregate_func=aggregate_method,
@@ -189,16 +196,15 @@ class DatasetService:
         dataset.save()
         return imputed_col_stats
 
-    def add_null(self,df):
+    def add_null(self, df):
         ix = [(row, col) for row in range(df.shape[0]) for col in range(df.shape[1])]
-        for row, col in random.sample(ix, int(round(.1*len(ix)))):
+        for row, col in random.sample(ix, int(round(0.1 * len(ix)))):
             df.iat[row, col] = np.nan
         return df
 
-    def calc_null(self,df):
-        null_val=df.isnull().sum().sum()
+    def calc_null(self, df):
+        null_val = df.isnull().sum().sum()
         print(null_val)
-
 
     def impute_dataset(self, dataset_id, target_col_name):
         dataset: Dataset = self.find_by_id(dataset_id, get_jwt_identity())
@@ -207,29 +213,30 @@ class DatasetService:
         )
         print("Auto Imputation")
         # print(dataset_frame)
-        features={}
-        features['rows_count']=dataset.info.tupleCount
-        dataset_fields=dataset.datasetFields
-        features['cols_count']=len(dataset_fields)
-        features['is_classification'],features['unique_ratio'],features['top_n_unique_ratio']=is_discrete_auto_impute(dataset_frame[target_col_name])
-        discrete_count=0
-        continous_count=0
-        null_count=0
-        dataset_frame=self.add_null(dataset_frame)
-        for i in range(features['cols_count']):
-            null_count+=dataset_fields[i]['metrics']['missingValues']
-            if(dataset_fields[i]['colType']==Coltype.DISCRETE):
-                discrete_count+=1
+        features = {}
+        features["rows_count"] = dataset.info.tupleCount
+        dataset_fields = dataset.datasetFields
+        features["cols_count"] = len(dataset_fields)
+        (
+            features["is_classification"],
+            features["unique_ratio"],
+            features["top_n_unique_ratio"],
+        ) = is_discrete_auto_impute(dataset_frame[target_col_name])
+        discrete_count = 0
+        continous_count = 0
+        null_count = 0
+        dataset_frame = self.add_null(dataset_frame)
+        for i in range(features["cols_count"]):
+            null_count += dataset_fields[i]["metrics"]["missingValues"]
+            if dataset_fields[i]["colType"] == Coltype.DISCRETE:
+                discrete_count += 1
             else:
-                continous_count+=1
-      
-        features['percent_null']=(null_count/features['rows_count'])*100
-        features['discrete_count']=discrete_count
-        features['continous_count']=continous_count
+                continous_count += 1
+
+        features["percent_null"] = (null_count / features["rows_count"]) * 100
+        features["discrete_count"] = discrete_count
+        features["continous_count"] = continous_count
         print(features)
         self.calc_null(dataset_frame)
         # ordinalEncoder = DataFrameOrdinalencoder(dataset_meta=dataset)
         # preprocessed_frame = ordinalEncoder.fit(dataset_frame)
-
-
-
