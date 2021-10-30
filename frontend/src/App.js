55d2cc85387de7cc4f0fac2eaeb8b90daf2a2cfd
Deepanshu Vangani <vanganideepanshu@gmail.com>
2021-10-30 17:16:30 +0530

    mock auth token setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,14 @@
 import "./App.scss";
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter } from "react-router-dom";
 import Layout from "./components/Layout";
 function App() {
+  useEffect(() => {
+    localStorage.setItem(
+      "auth_token",
+      "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTYzNTQ4NzQ4MiwianRpIjoiNjBlMjNkZmEtZmE3Ni00NmNjLWFlODctZGRlODhkZGMxYTY0IiwidHlwZSI6ImFjY2VzcyIsInN1YiI6IjYwZjkxOGVjYTU3MTRkZmY3YjdmN2VjOCIsIm5iZiI6MTYzNTQ4NzQ4MiwiZXhwIjoxNjM2MDkyMjgyfQ.nfFr3RKElOusDsvlrlSJL7keIFgPBHz68wsBGRVXee8"
+    );
+  }, []);
   return (
     <BrowserRouter>
       <Layout />
