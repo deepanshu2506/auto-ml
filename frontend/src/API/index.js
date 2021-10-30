8220f4f03e2892646af7dc24d3494b8cb5c0ad15
Deepanshu Vangani <vanganideepanshu@gmail.com>
2021-10-30 17:16:11 +0530

    add backend api axois config

---
+++ frontend/src/API/index.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+const endpoint = "http://localhost:5000";
+export const apiURLs = {
+  dataset: {
+    create: "/datasets/",
+  },
+};
+
+const addTokenToConfig = (config) => {
+  const Token = localStorage.getItem("auth_token");
+  if (Token) config.headers["Authorization"] = "Bearer " + Token;
+
+  return config;
+};
+
+const jsonAPI = axios.create({
+  baseURL: endpoint,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+const formDataAPI = axios.create({
+  baseURL: endpoint,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+jsonAPI.interceptors.request.use(addTokenToConfig, function (error) {
+  return Promise.reject(error);
+});
+
+formDataAPI.interceptors.request.use(addTokenToConfig, function (error) {
+  return Promise.reject(error);
+});
+
+const API = {
+  json: jsonAPI,
+  formData: formDataAPI,
+};
+export default API;
