0f379a7e01b9914536b4c6017a745aeeacafbd8f
hiral72 <46996903+hiral72@users.noreply.github.com>
2021-11-15 23:10:26 +0530

    api for dataset preview created

--- services/DatasetService.py
+++ services/DatasetService.py
@@ -141,12 +141,17 @@ class DatasetService:
         return Dataset.objects(createdBy=user_id, isDeleted=False).order_by(
             "-createdAt"
         )
-
+    def getDataset(self,id, user_id) -> DataFrame:
+        dataset = self.find_by_id(id, user_id)
+        dataset_frame: DataFrame = self.fileService.get_dataset_from_url(
+            dataset.datasetLocation
+        )        
+        return dataset_frame
     def find_by_id(self, id, user_id) -> Dataset:
         datasets = Dataset.objects(createdBy=user_id, id=id, isDeleted=False)
         if len(datasets) == 0:
             raise DatasetNotFound
-        return datasets[0]
+        return datasets[0]              
 
     def delete_dataset(self, id, user_id) -> None:
         dataset = self.find_by_id(id, user_id)
