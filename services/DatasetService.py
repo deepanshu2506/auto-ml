6ff317c93d7c92fcb68e5afc9b52d107d31e2f21
Deepanshu Vangani <vanganideepanshu@gmail.com>
2022-06-04 23:07:50 +0530

    migrate dataset service to metrics Extractor

--- services/DatasetService.py
+++ services/DatasetService.py
@@ -1,84 +1,26 @@
 from typing import Dict, List
 
 from lib.integrations.DataSourceFactory import DatasourceFactory
+from lib.operations.MetricsExtractor.MetricsExtractor import DatasetMetricsExtractor
 from lib.preprocessing import (
     clean_col_names,
-    get_outlier_count,
-    get_samples,
-    get_value_percentage_dict,
     replace_boolean,
     replace_nulls,
 )
-from utils.enums import AggregationMethods, Coltype, DataTypes, DatasetType
+from utils.enums import AggregationMethods, Coltype, DatasetType
 from utils.exceptions import DatasetNotFound
 import numpy
-from pandas.core.series import Series
-
-from utils.pdUtils import build_query, get_col_type, get_datatype, perform_aggregation
+from utils.pdUtils import build_query, perform_aggregation
 from flask_jwt_extended.utils import get_jwt_identity
 from services.FileService import FileService
 from pandas import DataFrame
 from werkzeug.datastructures import FileStorage
-from db.models.Dataset import (
-    Dataset,
-    DatasetFeature,
-    DatasetFeatureMetrics,
-    DatasetInfo,
-)
+from db.models.Dataset import Dataset, DatasetFeature
 
 
 class DatasetService:
     def __init__(self, fileService: FileService) -> None:
         self.fileService = fileService
-
-    def _build_column_metrics(
-        self, column_metrics: Series, column_values: Series, colType: Coltype
-    ):
-        featureMetrics = DatasetFeatureMetrics()
-
-        if colType == Coltype.CONTINOUS:
-            featureMetrics.outlier_count = get_outlier_count(column_values)
-
-            featureMetrics.min = column_metrics["min"]
-            featureMetrics.max = column_metrics["max"]
-            featureMetrics.mean = column_metrics["mean"]
-            featureMetrics.stdDeviation = column_metrics["std"]
-            featureMetrics.median = numpy.nanmedian(column_values)
-        else:
-            featureMetrics.value_percentage = get_value_percentage_dict(column_values)
-
-        unique_values = column_values.unique().tolist()
-        featureMetrics.uniqueValues = len(unique_values)
-        featureMetrics.samples = get_samples(unique_values, 5)
-        featureMetrics.missingValues = column_values.isnull().sum()
-        return featureMetrics
-
-    def _extract_fields(self, dataset: DataFrame):
-        datasetFields: List[DatasetFeature] = []
-        dataTypes = dataset.dtypes
-        columnNames = dataset.columns.values.tolist()
-        dataset_metrics = dataset.describe()
-        for idx, (columnName, dataType) in enumerate(zip(columnNames, dataTypes)):
-            colDataType: DataTypes = get_datatype(dataType)
-            colData: Series = dataset.iloc[:, idx]
-            colType: Coltype = get_col_type(colData, colDataType)
-            datasetFeature = DatasetFeature(
-                columnOrder=idx + 1,
-                columnName=columnName,
-                dataType=colDataType,
-                colType=colType,
-            )
-            cols_with_metrics: list = dataset_metrics.columns.tolist()
-            raw_column_metrics = (
-                dataset_metrics.iloc[:, cols_with_metrics.index(columnName)]
-                if columnName in cols_with_metrics or colType == Coltype.CONTINOUS
-                else {}
-            )
-            datasetFeature.metrics = self._build_column_metrics(
-                raw_column_metrics, colData, colType
-            )
-            datasetFields.append(datasetFeature)
-        return datasetFields
 
     def createDataset(
         self,
@@ -119,15 +61,17 @@ class DatasetService:
         dataset_raw = clean_col_names(dataset_raw)
         dataset_raw = replace_boolean(dataset_raw)
 
-        dataset.datasetFields = self._extract_fields(dataset_raw)
+        metricsExtractor = DatasetMetricsExtractor()
+        dataset.datasetFields = metricsExtractor.extract_feature_metrics(dataset_raw)
 
         dataset = dataset.save()
         file_path, file_size = self.fileService.save_dataset(
             dataset_raw, user_id=user_id, dataset_id=dataset.id
         )
-        tupleCount = len(dataset_raw.index)
         dataset.datasetLocation = file_path
-        dataset.info = DatasetInfo(fileSize=file_size, tupleCount=tupleCount)
+        datasetInfo = metricsExtractor.extract_dataset_metrics(dataset_raw)
+        datasetInfo.fileSize = file_size
+        dataset.info = datasetInfo
         dataset = dataset.save()
         return dataset.id
 
